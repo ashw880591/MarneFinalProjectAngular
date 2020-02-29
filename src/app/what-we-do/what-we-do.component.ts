cea9a91aa0e1f215b2db2f48cb17c807df6ac9f7
ashw880591 <ashwinisuryawanshi901@gmail.com>
2020-03-01 00:54:10 +0530

    updated npm configurations.

--- src/app/what-we-do/what-we-do.component.ts
+++ src/app/what-we-do/what-we-do.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-what-we-do',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhatWeDoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router) { }
 
+  GoToOurProject(){
+this.route.navigate(['OurProject'])
+  }
   ngOnInit() {
     // this.loadAllImageDiv();
   }
